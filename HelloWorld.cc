8063e23795529457337eb7acb5f1dc98832fb2f9
Julia D. Hewitt <juliahewitt@storm.cis.fordham.edu>
2016-02-01 21:07:49 -0500

    Hello

---
+++ HelloWorld.cc
@@ -0,0 +1,22 @@
+// cin with strings 
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main ()
+{
+
+string a;
+cin >> a;
+cout << "Hello, my name is " << a << endl;
+
+string b;
+cin >> b;
+cout << "I am " << b << endl;
+
+string c;
+cin >> c;
+cout << "My favorite animal is " << c << endl;
+
+}
